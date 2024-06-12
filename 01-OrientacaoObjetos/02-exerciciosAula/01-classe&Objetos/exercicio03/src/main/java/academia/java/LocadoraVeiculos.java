3fa321bbae23d2cedb19c4dd854b817141497c84
Gláucia Andare <gandare@gmail.com>
2024-06-12 13:10:40 -0300

    Corrigindo nome da Classe

---
+++ 01-OrientacaoObjetos/02-exerciciosAula/01-classe&Objetos/exercicio03/src/main/java/academia/java/LocadoraVeiculos.java
@@ -0,0 +1,9 @@
+package academia.java;
+
+public class LocadoraVeiculos {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        carro.configurarDados("VW", "Gol");
+        carro.exibirDados();
+    }
+}
