68956f35acf3c0e4b9e9e1f7950ef52821be7e15
Gláucia Andare <gandare@gmail.com>
2024-06-12 19:56:03 -0300

    Finalizando Exerício

--- 01-OrientacaoObjetos/02-exerciciosAula/02-Construtores/exercicio02/src/main/java/academia/java/Aluno.java
+++ 01-OrientacaoObjetos/02-exerciciosAula/02-Construtores/exercicio02/src/main/java/academia/java/Aluno.java
@@ -2,4 +2,32 @@ package academia.java;
 
 public class Aluno {
 
+    private String nome;
+    private int matricula;
+    private String dataNascimento;
+    private int anoIngresso;
+
+    public Aluno(String nome, int matricula) {
+        this.nome = nome;
+        this.matricula = matricula;
+    }
+
+    public Aluno(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Aluno(String dataNascimento, String nome, int anoIngresso) {
+        this.dataNascimento = dataNascimento;
+        this.nome = nome;
+        this.anoIngresso = anoIngresso;
+    }
+
+    public String toString() {
+        return  "{" +
+                "nome='" + nome + '\'' +
+                ", matricula='" + matricula + '\'' +
+                ", dataNascimento='" + dataNascimento + '\'' +
+                ", anoIngresso=" + anoIngresso +
+                '}';
+    }
 }
--- 01-OrientacaoObjetos/02-exerciciosAula/02-Construtores/exercicio02/src/main/java/academia/java/Main.java
+++ 01-OrientacaoObjetos/02-exerciciosAula/02-Construtores/exercicio02/src/main/java/academia/java/Main.java
@@ -2,6 +2,14 @@ package academia.java;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Aluno aluno1 = new Aluno("Cecília", 123456);
+        Aluno aluno2 = new Aluno("20/11/1969");
+        Aluno aluno3 = new Aluno("Vanessa", "02/05/1989", 2024);
+
+        System.out.println("Aluno1" + aluno1);
+        System.out.println("Aluno2" + aluno2);
+        System.out.println("Aluno3" + aluno3);
+
     }
 }
